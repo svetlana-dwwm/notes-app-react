2a4ed6f9168df5b83631e45f974d004c146113af
svetlana-dwwm <150051447+svetlana-dwwm@users.noreply.github.com>
2024-03-04 09:12:01 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,4 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  base: '/notes-app-react/',
-  plugins: [react()]
-})
+export default {
+  base: '/notes-app-react/'
+}
 
